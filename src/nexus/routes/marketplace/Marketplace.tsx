4007484c0a7e1f5dc9479f8550ebaa2799d0a642
Aaron <aarontburnham@hotmail.com>
2025-05-17 20:52:46 -0700

    yea

--- src/nexus/routes/marketplace/Marketplace.tsx
+++ src/nexus/routes/marketplace/Marketplace.tsx
@@ -1,27 +1,36 @@
+import { useState } from "react";
 import "./marketplace.css"
 import monkeyImg from './spotify-monkey.png';
+import { NexusDatabase } from "./db";
+
+new NexusDatabase()
 
 export default function NexusMarket() {
+    const [searchQuery, setSearchQuery] = useState<string>()
+
+
     return <div className="page">
         <div className="body mbody">
             <div className="left"></div>
             <div className="right"></div>
             <div className="main">
                 <h2>All Modules</h2>
+                
                 <input type="text" />
 
 
                 <div id="module-container">
-                    <Module />
-                    <Module />
-                    <Module />
-                    <Module />
-                    <Module />
-                    <Module />
-                    <Module />
+                    <Module
+                        name="Debug Console"
+                        shortDesc="Debug and view console outputs within Nexus."
+                        author="aarontburn" />
+
+                    <Module
+                        name="Spotify Monkey"
+                        author="aarontburn" />
+
+
                 </div>
-
-
 
             </div>
 
@@ -30,13 +39,23 @@ export default function NexusMarket() {
 }
 
 
-function Module() {
+interface ModuleProps {
+    name: string;
+    author: string;
+    shortDesc?: string;
+    image?: string;
+
+}
+
+function Module({ name, shortDesc, image, author }: ModuleProps) {
     return <div className="module">
         <div className="module-image-container">
             <img src={monkeyImg} />
         </div>
-        <div className="module-desc-container">
-            <h3>Spotify Monkey</h3>
+        <div className="module-info-container">
+            <h3 className="module-info-name">{name}</h3>
+            <h4>{author}</h4>
+            {shortDesc && <h4>{shortDesc}</h4>}
         </div>
 
     </div>
